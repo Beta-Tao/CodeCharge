28549871e53d7df62b922c0b12a5fa12a987c42e
Beta_Tao <wangtao_0902@163.com>
2021-11-09 13:35:14 +0800

    Add offer_20

--- leetcode/editor/cn/Offer_20_biao-shi-shu-zhi-de-zi-fu-chuan-lcof.cpp
+++ leetcode/editor/cn/Offer_20_biao-shi-shu-zhi-de-zi-fu-chuan-lcof.cpp
@@ -2,50 +2,141 @@
 using namespace std;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution
+{
 public:
-    bool isNumber(string s) {
+    enum State {
+        STATE_INITIAL,
+        STATE_INT_SIGN,
+        STATE_INTEGER,
+        STATE_POINT,
+        STATE_POINT_WITHOUT_INT,
+        STATE_FRACTION,
+        STATE_EXP,
+        STATE_EXP_SIGN,
+        STATE_EXP_NUMBER,
+        STATE_END,
+    };
 
-    }
-
-    bool isInteger(string s)
+    enum CharType
     {
-        if (s[0] != '+' && s[0] != '-' && (s[0] < '0' || s[0] > '9'))
-        {
-            return false;
-        }
-        for (int i = 1; i < s.length(); i++)
-        {
-            if (s[0] < '0' || s[0] > '9')
-                return false;
-        }
-        return true;
-    }
+        CHAR_NUMBER,
+        CHAR_EXP,
+        CHAR_POINT,
+        CHAR_SIGN,
+        CHAR_SPACE,
+        CHAR_ILLEGAL,
+    };
 
-    bool isDecimal(string s)
+    CharType toCharType(const char &ch)
     {
-        auto pos = s.find('.');
-        if (pos == s.npos)      // 不含小数点
-            return false;
-        else if (pos == 0)
-        {
-            return isInteger()
-        }
+        if (ch >= '0' && ch <= '9')
+            return CHAR_NUMBER;
+        else if (ch == 'e' || ch == 'E')
+            return CHAR_EXP;
+        else if (ch == '.')
+            return CHAR_POINT;
+        else if (ch == '+' || ch == '-')
+            return CHAR_SIGN;
+        else if (ch == ' ')
+            return CHAR_SPACE;
         else
+            return CHAR_ILLEGAL;
+    }
+
+    bool isNumber(string s){
+        unordered_map<State, unordered_map<CharType, State>> transfer{
+            {
+                STATE_INITIAL,
+                {
+                    {CHAR_SPACE, STATE_INITIAL},
+                    {CHAR_NUMBER, STATE_INTEGER},
+                    {CHAR_POINT, STATE_POINT_WITHOUT_INT},
+                    {CHAR_SIGN, STATE_INT_SIGN}
+                }
+            },
+            {
+                STATE_INT_SIGN,
+                {
+                    {CHAR_NUMBER, STATE_INTEGER},
+                    {CHAR_POINT, STATE_POINT_WITHOUT_INT}
+                }
+            },
+            {
+                STATE_INTEGER,
+                {
+                    {CHAR_NUMBER, STATE_INTEGER},
+                    {CHAR_POINT, STATE_POINT},
+                    {CHAR_EXP, STATE_EXP},
+                    {CHAR_SPACE, STATE_END}
+                }
+            },
+            {
+                STATE_POINT_WITHOUT_INT,
+                {
+                    {CHAR_NUMBER, STATE_FRACTION}
+                }
+            },
+            {
+                STATE_POINT,
+                {
+                    {CHAR_NUMBER, STATE_FRACTION},
+                    {CHAR_EXP, STATE_EXP},
+                    {CHAR_SPACE, STATE_END}
+                }
+            },
+            {
+                STATE_FRACTION,
+                {
+                    {CHAR_NUMBER, STATE_FRACTION},
+                    {CHAR_EXP, STATE_EXP},
+                    {CHAR_SPACE, STATE_END}
+                }
+            },
+            {
+                STATE_EXP,
+                {
+                    {CHAR_SIGN, STATE_EXP_SIGN},
+                    {CHAR_NUMBER, STATE_EXP_NUMBER}
+                }
+            },
+            {
+                STATE_EXP_SIGN,
+                {
+                    {CHAR_NUMBER, STATE_EXP_NUMBER}
+                }
+            },
+            {
+                STATE_EXP_NUMBER,
+                {
+                    {CHAR_NUMBER, STATE_EXP_NUMBER},
+                    {CHAR_SPACE, STATE_END}
+                }
+            },
+            {
+                STATE_END,
+                {
+                    {CHAR_SPACE, STATE_END}
+                }
+            }
+        };
+
+        int len = s.length();
+        State state = STATE_INITIAL;
+
+        for (int i = 0; i < len; i++)
         {
-            if (s[0] != '+' && s[0] != '-' && (s[0] < '0' || s[0] > '9'))
-            {
+            CharType char_type = toCharType(s[i]);
+            if (transfer[state].find(char_type) == transfer[state].end())
                 return false;
-            }
-            for (int i = 1; i < s.length(); i++)
-            {
-                if (i = )
-            }
+            else
+                state = transfer[state][char_type];
         }
+        return state == STATE_INTEGER || state ==  STATE_POINT || state == STATE_FRACTION ||
+               state == STATE_EXP_NUMBER || state == STATE_END;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
-
 
 int main()
 {
